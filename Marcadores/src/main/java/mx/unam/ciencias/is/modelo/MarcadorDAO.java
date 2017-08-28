bde6dbc7b432ccc05c25bc82d35dd7cb5aa04ffd
lngallo <trickzon.k@gmail.com>
2017-08-28 16:29:57 -0500

    Se completaron los métodos de la clase MarcadorDAO

--- Marcadores/src/main/java/mx/unam/ciencias/is/modelo/MarcadorDAO.java
+++ Marcadores/src/main/java/mx/unam/ciencias/is/modelo/MarcadorDAO.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package mx.unam.ciencias.is.modelo;
+
 import mx.unam.ciencias.is.mapeobd.Marcador;
 import java.util.List;
 import org.hibernate.Query;
@@ -16,21 +17,22 @@ import org.hibernate.Transaction;
  * @author jonathan
  */
 public class MarcadorDAO {
-    
+
     /*Sesion para conectarnos a la base de datos*/
     private SessionFactory sessionFactory;
-    
+
     /**
      * Inicialisamos la sesion a la base de datos.
-     * @param sessionFactory 
+     *
+     * @param sessionFactory
      */
     public void setSessionFactory(SessionFactory sessionFactory) {
         this.sessionFactory = sessionFactory;
     }
-    
-    
+
     /**
-     * Guarda un marcador a la base de datos 
+     * Guarda un marcador a la base de datos
+     *
      * @param marcador el marcador a guardar.
      */
     public void guardar(Marcador marcador) {
@@ -42,30 +44,29 @@ public class MarcadorDAO {
             tx = session.beginTransaction();
             //guardamos el marcador
             session.persist(marcador);
-           
+
             tx.commit();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             //Se regresa a un estado consistente 
-            if (tx!=null){ 
+            if (tx != null) {
                 tx.rollback();
             }
-            e.printStackTrace(); 
-        }
-        finally {
+            e.printStackTrace();
+        } finally {
             //cerramos simpre la sesion
             session.close();
         }
-    
+
     }
-    
+
     /**
      * Regresa la lista de todos los marcadores en la base de datos
+     *
      * @return la lista que contiene a todos los marcadores de la base de datos
      */
-    public List<Marcador> getMarcadores(){
+    public List<Marcador> getMarcadores() {
         //Aqui va tu codigo
-        List<Marcador> result = null;        
+        List<Marcador> result = null;
         //se inicia la sesion
         Session session = sessionFactory.openSession();
         //la transaccion a relizar
@@ -74,67 +75,100 @@ public class MarcadorDAO {
             tx = session.beginTransaction();
             String hql = "FROM Marcador";
             Query query = session.createQuery(hql);
-            result = (List<Marcador>)query.list();
+            result = (List<Marcador>) query.list();
             tx.commit();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             //Se regresa a un estado consistente 
-            if (tx!=null){ 
+            if (tx != null) {
                 tx.rollback();
             }
-            e.printStackTrace(); 
-        }
-        finally {
+            e.printStackTrace();
+        } finally {
             //cerramos simpre la sesion
             session.close();
         }
+        return result;
     }
-    
+
     /**
-     * Regresa el marcador con la longitud  y latitud dada. 
+     * Regresa el marcador con la longitud y latitud dada.
+     *
      * @param lattitud
      * @param longitud
      * @return el marcador con la longitud y latitud dada.
      */
-    public Marcador getMarcador(double latitud,double longitud) {
+    public Marcador getMarcador(double latitud, double longitud) {
         //aqui va tu codigo
+        //creamos una variable en donde guardar el marcador
+        Marcador marcador = null;
         //se inicia la sesion
         Session session = sessionFactory.openSession();
         //la transaccion a relizar
         Transaction tx = null;
         try {
             tx = session.beginTransaction();
-            String hql = "FROM Mar";
+            String hql = "FROM Marcador WHERE latitud = :latitud AND longitud = :longitud";
             Query query = session.createQuery(hql);
-            result = (List<Marcador>)query.list();
+            query.setParameter("latitud", latitud);
+            query.setParameter("longitud", longitud);
+            
+            //Hacemos un cast de lo que obtenemos en el query a un obj Marcador
+            marcador = (Marcador) query.uniqueResult();
+            
             tx.commit();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             //Se regresa a un estado consistente 
-            if (tx!=null){ 
+            if (tx != null) {
                 tx.rollback();
             }
-            e.printStackTrace(); 
-        }
-        finally {
+            e.printStackTrace();
+        } finally {
             //cerramos simpre la sesion
             session.close();
         }
+        return marcador;
     }
-    
+
     /**
      * Regresa el marcador con el id dado
-     * @param id del marcador 
+     *
+     * @param id del marcador
      * @return el marcador con ese id
      */
     public Marcador getMarcadorId(int id) {
         //aqui va tu codigo
-        
+        //creamos una variable en donde guardar el marcador
+        Marcador marcador = null;
+        //se inicia la sesion
+        Session session = sessionFactory.openSession();
+        //la transaccion a relizar
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            String hql = "FROM Marcador WHERE marcador_id = :idm";
+            Query query = session.createQuery(hql);
+            query.setParameter("idm", id);
+            
+            //Hacemos un cast de lo que obtenemos en el query a un obj Marcador
+            marcador = (Marcador) query.uniqueResult();
+            
+            tx.commit();
+        } catch (Exception e) {
+            //Se regresa a un estado consistente 
+            if (tx != null) {
+                tx.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            //cerramos simpre la sesion
+            session.close();
+        }
+        return marcador;
     }
-    
-    
+
     /**
      * Elimina el marcador de la base de datos
+     *
      * @param marcador el marcador a eliminar
      */
     public void eliminar(Marcador marcador) {
@@ -147,25 +181,24 @@ public class MarcadorDAO {
             tx = session.beginTransaction();
             //guardamos el marcador
             session.delete(marcador);
-        
+
             tx.commit();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             //Se regresa a un estado consistente 
-            if (tx!=null){ 
+            if (tx != null) {
                 tx.rollback();
             }
-            e.printStackTrace(); 
-        }
-        finally {
+            e.printStackTrace();
+        } finally {
             //cerramos simpre la sesion
             session.close();
         }
     }
-    
+
     /**
      * Actualiza el marcardor en la base de datos
-     * @param marcador con los nuevos valores 
+     *
+     * @param marcador con los nuevos valores
      */
     public void actualizar(Marcador marcador) {
         //aqui va tu codigo
@@ -177,17 +210,15 @@ public class MarcadorDAO {
             tx = session.beginTransaction();
             //guardamos el marcador
             session.update(marcador);
-           
+
             tx.commit();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             //Se regresa a un estado consistente 
-            if (tx!=null){ 
+            if (tx != null) {
                 tx.rollback();
             }
-            e.printStackTrace(); 
-        }
-        finally {
+            e.printStackTrace();
+        } finally {
             //cerramos simpre la sesion
             session.close();
         }
